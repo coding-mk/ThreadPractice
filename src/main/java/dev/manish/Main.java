2b2dbbf3890644a56b9b3b81ea82ff400c2e1bb4
ManishKshetrapal45 <manishkshetrapal105@gmail.com>
2026-07-15 19:49:40 +0530

    Code to print hello world

--- src/main/java/dev/manish/Main.java
+++ src/main/java/dev/manish/Main.java
@@ -2,6 +2,6 @@ package dev.manish;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!, Print by  a thread: " + Thread.currentThread().getName());
     }
 }
